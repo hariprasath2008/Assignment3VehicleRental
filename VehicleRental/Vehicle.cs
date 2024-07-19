b62289df5d2d1856302b551ccb94de1701b1a145
Hari Prasath Muthurathinam <hmuthurathinam0672@conestogac.on.ca>
2024-07-19 12:19:39 -0400

    Base Classes Created
    Classes for vehicle,truck,car and motorcycle has been created

---
+++ VehicleRental/Vehicle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public class Vehicle
+{
+    public string Model { get; set; }
+    public string Manufacturer { get; set; }
+    public int Year { get; set; }
+    public decimal RentalPrice { get; set; }
+
+    public Vehicle(string model, string manufacturer, int year, decimal rentalPrice)
+    {
+        Model = model;
+        Manufacturer = manufacturer;
+        Year = year;
+        RentalPrice = rentalPrice;
+    }
+
+    public virtual void DisplayDetails()
+    {
+        Console.WriteLine($"<-----Model: {Model}, Manufacturer: {Manufacturer}, Year: {Year}, Rental Price: {RentalPrice}");
+    }
+}
