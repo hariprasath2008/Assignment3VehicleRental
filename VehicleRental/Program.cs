5ec7ad29d06da16370d4b9a9e8dabea667d4493e
Hari Prasath Muthurathinam <hmuthurathinam0672@conestogac.on.ca>
2024-07-19 13:36:07 -0400

    Code completed with UI
    Code design has been completed successfully

--- VehicleRental/Program.cs
+++ VehicleRental/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace VehicleRental
+{
+    public static class VehicleRental
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hi.Welcome to Vehicle Rental Management System!");
+
+            RentalAgency agency = new RentalAgency();
+
+            Truck truck1 = new Truck("F-150", "Ford", 2019, 130, 2000, "Pickup", true);
+            Car car1 = new Car("Civic", "Honda", 2018, 83, 5, "Petrol", "Automatic", true);
+            Car car2 = new Car("Challenger", "Dodge", 2022, 100, 5, "Diesel", "Manual", false);
+            Motorcycle motorcycle1 = new Motorcycle("Ninja", "Kawasaki", 2018, 60, 1000, "Petrol", true);
+
+            agency.AddVehicle(truck1);
+            agency.AddVehicle(car1);
+            agency.AddVehicle(car2);
+            agency.AddVehicle(motorcycle1);
+
+            Console.WriteLine("Fleet details:");
+            agency.DisplayFleet();
+
+            agency.RentVehicle(car1, 3);
+            Console.WriteLine($"\n*********** Total Revenue: {agency.TotalRevenue} ***********");
+
+            Console.WriteLine("\nFleet details after renting a car:");
+            agency.DisplayFleet();
+        }
+    }
+}
---
+++ VehicleRental/RentalAgency.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class RentalAgency
+{
+    private List<Vehicle> Fleet { get; set; }
+    public decimal TotalRevenue { get; private set; }
+
+    public RentalAgency()
+    {
+        Fleet = new List<Vehicle>();
+        TotalRevenue = 0;
+    }
+
+    public void AddVehicle(Vehicle vehicle)
+    {
+        Fleet.Add(vehicle);
+    }
+
+    public void RemoveVehicle(Vehicle vehicle)
+    {
+        Fleet.Remove(vehicle);
+    }
+
+    public void RentVehicle(Vehicle vehicle, int days)
+    {
+        if (Fleet.Contains(vehicle))
+        {
+            Fleet.Remove(vehicle);
+            decimal rentalCost = vehicle.RentalPrice * days;
+            TotalRevenue += rentalCost;
+            Console.WriteLine();
+            Console.WriteLine($"---->Rented {vehicle.Manufacturer} {vehicle.Model} for {days} days. Cost: ${rentalCost}");
+        }
+        else
+        {
+            Console.WriteLine("Vehicle not available for rent.");
+        }
+    }
+
+    public void DisplayFleet()
+    {
+        foreach (var vehicle in Fleet)
+        {
+            vehicle.DisplayDetails();
+            Console.WriteLine(".............");
+        }
+    }
+}
